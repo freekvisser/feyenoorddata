1580bd9782cf1599b9038cb69eca7aa641bf8968
Freek Visser <freek@visser.fo>
2022-06-03 18:26:17 +0200

    first version of two-way communication between HTML/JS and Flask

--- index.py
+++ index.py
@@ -1,10 +1,23 @@
+import flask
 from flask import Flask, render_template
 
 app = Flask(__name__)
+
+app.secret_key = 'SOME_SECRET_KEY'
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+#background process happening without any refreshing
+@app.route('/test')
+def background_process_test():
+    player = flask.request.args.get('player')
+    flask.session['player'] = player
+    return flask.jsonify({"response": player})
+
+
+
 
 app.run(host='0.0.0.0', port=8000, debug=True)
