7c8651ae1de9290e60b3ccd2c52221707ad5a95a
Freek Visser <freek@visser.fo>
2022-06-09 11:53:29 +0200

    add off target stat and interactivity

--- shots.py
+++ shots.py
@@ -10,6 +10,8 @@ class Shots:
         fig, ax = pitch.draw()
 
         shots = []
+        offTarget = []
+        blocked = []
         onTarget = []
         goals = []
 
@@ -26,13 +28,22 @@ class Shots:
                 goals.append(data)
             elif shot['outcome']['name'] == 'Saved':
                 onTarget.append(data)
+            elif shot['outcome']['name'] == 'Blocked':
+                blocked.append(data)
+            elif shot['outcome']['name'] == 'Off T' or shot['outcome']['name'] == 'Wayward':
+                offTarget.append(data)
             else:
                 shots.append(data)
 
         for shot in shots:
             pitch.scatter(shot['location'][0], shot['location'][1], s=(shot['xG'] * 900) + 100, marker='^',
                           edgecolors='black', c='#ff0000', alpha=0.7, ax=ax)
-
+        for shot in offTarget:
+            pitch.scatter(shot['location'][0], shot['location'][1], s=(shot['xG'] * 900) + 100, marker='^',
+                          edgecolors='black', c='#ff0000', alpha=0.7, ax=ax)
+        for shot in blocked:
+            pitch.scatter(shot['location'][0], shot['location'][1], s=(shot['xG'] * 900) + 100, marker='^',
+                          edgecolors='black', c='#ff0000', alpha=0.7, ax=ax)
         for shot in onTarget:
             pitch.scatter(shot['location'][0], shot['location'][1], s=(shot['xG'] * 900) + 100, marker='s',
                           edgecolors='black', c='#0000FF', alpha=0.7, ax=ax)
@@ -53,7 +64,8 @@ class Shots:
             # get number of shots by getting number of rows
             'total': data.shape[0],
             'on_target': 0,
-            'blocked': 0
+            'blocked': 0,
+            'off_target': 0
         }
 
         for index, shot in data.iterrows():
@@ -66,6 +78,8 @@ class Shots:
                 attempts['on_target'] = attempts['on_target'] + 1
             elif shotOutcome == 'Blocked':
                 attempts['blocked'] = attempts['blocked'] + 1
+            elif shotOutcome == 'Off T' or shotOutcome == 'Wayward':
+                attempts['off_target'] = attempts['off_target'] + 1
 
         return {
             'xG': round(xGs, 2),
