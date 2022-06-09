4bc666cc2e01a0ee8aee9b82800fdb89061f230f
Freek Visser <freek@visser.fo>
2022-06-10 00:12:39 +0200

    add lines directing to shots' end location

--- shots.py
+++ shots.py
@@ -12,6 +12,7 @@ class Shots:
         shots = []
         offTarget = []
         blocked = []
+        post = []
         onTarget = []
         goals = []
 
@@ -19,9 +20,11 @@ class Shots:
             shot = row['shot']
             xG = shot['statsbomb_xg']
             location = row['location']
+            end_location = shot['end_location']
             data = {
                 'xG': xG,
-                'location': location
+                'location': location,
+                'end_location': end_location,
             }
 
             if shot['outcome']['name'] == 'Goal':
@@ -32,25 +35,48 @@ class Shots:
                 blocked.append(data)
             elif shot['outcome']['name'] == 'Off T' or shot['outcome']['name'] == 'Wayward':
                 offTarget.append(data)
+            elif shot['outcome']['name'] == 'Post':
+                post.append(data)
             else:
                 shots.append(data)
 
         for shot in shots:
             pitch.scatter(shot['location'][0], shot['location'][1], s=(shot['xG'] * 900) + 100, marker='^',
                           edgecolors='black', c='#ff0000', alpha=0.7, ax=ax)
+            pitch.arrows(shot['location'][0], shot['location'][1], shot['end_location'][0], shot['end_location'][1],
+                         ax=ax)
         for shot in offTarget:
             pitch.scatter(shot['location'][0], shot['location'][1], s=(shot['xG'] * 900) + 100, marker='^',
-                          edgecolors='black', c='#ff0000', alpha=0.7, ax=ax)
+                          edgecolors='black', c='#FF0000', alpha=0.7, ax=ax)
+            pitch.arrows(shot['location'][0], shot['location'][1], shot['end_location'][0], shot['end_location'][1],
+                         width=2, headwidth=10, headlength=10, color='#FF0000',
+                         ax=ax)
         for shot in blocked:
             pitch.scatter(shot['location'][0], shot['location'][1], s=(shot['xG'] * 900) + 100, marker='^',
-                          edgecolors='black', c='#ff0000', alpha=0.7, ax=ax)
+                          edgecolors='black', c='#FF0000', alpha=0.7, ax=ax)
+            pitch.arrows(shot['location'][0], shot['location'][1], shot['end_location'][0], shot['end_location'][1],
+                         width=2, headwidth=10, headlength=10, color='#FF0000',
+                         ax=ax)
+        for shot in post:
+            pitch.scatter(shot['location'][0], shot['location'][1], s=(shot['xG'] * 900) + 100, marker='s',
+                          edgecolors='black', c='#FFFF00', alpha=0.7, ax=ax)
+            pitch.arrows(shot['location'][0], shot['location'][1], shot['end_location'][0], shot['end_location'][1],
+                         width=2, headwidth=10, headlength=10, color='#FFFF00',
+                         ax=ax)
         for shot in onTarget:
             pitch.scatter(shot['location'][0], shot['location'][1], s=(shot['xG'] * 900) + 100, marker='s',
                           edgecolors='black', c='#0000FF', alpha=0.7, ax=ax)
-
+            pitch.arrows(shot['location'][0], shot['location'][1], shot['end_location'][0], shot['end_location'][1],
+                         width=2,headwidth=10, headlength=10, color='#0000FF',
+                         ax=ax)
         for shot in goals:
             pitch.scatter(shot['location'][0], shot['location'][1], s=(shot['xG'] * 900) + 100, marker='h',
                           edgecolors='black', c='#00FF00', alpha=0.7, ax=ax)
+            pitch.arrows(shot['location'][0], shot['location'][1], shot['end_location'][0], shot['end_location'][1],
+                         width=2, headwidth=10, headlength=10, color='#00FF00',
+                         ax=ax)
+
+
 
         return fig, ax
 
@@ -64,6 +90,7 @@ class Shots:
             # get number of shots by getting number of rows
             'total': data.shape[0],
             'on_target': 0,
+            'post': 0,
             'blocked': 0,
             'off_target': 0
         }
@@ -80,6 +107,8 @@ class Shots:
                 attempts['blocked'] = attempts['blocked'] + 1
             elif shotOutcome == 'Off T' or shotOutcome == 'Wayward':
                 attempts['off_target'] = attempts['off_target'] + 1
+            elif shotOutcome == 'Post':
+                attempts['post'] = attempts['post'] + 1
 
         return {
             'xG': round(xGs, 2),
